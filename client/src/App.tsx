ccabddb90ee5e2902d547a29f630d99dffa99eb7
Maksim <maksim@Maksims-MacBook-Pro.local>
2023-01-23 21:18:18 +0300

    Client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {logo} from "./assets/logo.svg";
+import {Home, Register, Login} from "./pages";
+
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+        Hello World!
+    </div>
+  )
+}
+
+export default App
